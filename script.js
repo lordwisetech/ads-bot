176342239bb262d7a4ae56f55bd1063ae3782792
lordwisetech <104361385+lordwisetech@users.noreply.github.com>
2024-11-04 08:04:12 +0100

    Update script.js
    Done

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let accessKey = 'bot'; // Set your access key here
-let ads = ["https://www.profitablecpmrate.com/pmy3b5m27r?key=a1a01ca4cf215de9523bf26ddc5532a8",
-    "https://www.profitablecpmrate.com/zzse4wbzy1?key=05a1b5039f489d7a0d56be1584005f1f",
-    "https://www.profitablecpmrate.com/xenzs7swd4?key=5c2f163830246a41565b5d8c2bd694cc",
-    "https://www.profitablecpmrate.com/zb7muzm5yw?key=5d840a68ab542f32850f5f1ec5bcc061",
-"https://www.profitablecpmrate.com/j3d6dv5s?key=3af9ee2141841b4a86a8303c7873c4aa",
-"https://www.profitablecpmrate.com/xjwhfrene?key=b90e7ca701cbac772a9bae4c8ab9f01f"];
+let ads = ["https://www.profitablecpmrate.com/pmy3b5m27r?key=a1a01ca4cf215de9523bf26ddc5532a8"];
 let currentAdIndex = 0;
 let countdownInterval;
 
