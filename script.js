809e3b5c7346075b44c579cf909ecdd28d50e4f7
lordwisetech <104361385+lordwisetech@users.noreply.github.com>
2024-11-05 16:55:32 +0100

    Update script.js
    Limit the links to two

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 let accessKey = 'bot'; // Set your access key here
 let ads = [
     "https://www.profitablecpmrate.com/b8n9m5k1?key=49d1b07c7f86d580f8d82619ffc2c9c7",
-    "https://www.profitablecpmrate.com/b6gkcf2aa8?key=a342eb1837eb747b7a48c4a55ca5dc66",
-    "https://www.profitablecpmrate.com/pmy3b5m27r?key=a1a01ca4cf215de9523bf26ddc5532a8",
-    "https://www.profitablecpmrate.com/zzse4wbzy1?key=05a1b5039f489d7a0d56be1584005f1f",
-    "https://www.profitablecpmrate.com/zb7muzm5yw?key=5d840a68ab542f32850f5f1ec5bcc061",
-    "https://www.profitablecpmrate.com/j3d6dv5s?key=3af9ee2141841b4a86a8303c7873c4aa",
-    "https://www.profitablecpmrate.com/xjwhfrene?key=b90e7ca701cbac772a9bae4c8ab9f01f"
-];
+    "https://www.profitablecpmrate.com/b6gkcf2aa8?key=a342eb1837eb747b7a48c4a55ca5dc66"];
 
 
 
